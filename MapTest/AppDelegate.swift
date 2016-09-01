ace5281de83235506d891d1cad4b3128d54542d2
Kevin Barabash <kevinb7@gmail.com>
2016-09-01 11:48:18 -0700

    Initial Commit

---
+++ MapTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MapTest
+//
+//  Created by Kevin Barabash on 2016-09-01.
+//  Copyright © 2016 Kevin Barabash. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MapTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MapTest
+//
+//  Created by Kevin Barabash on 2016-09-01.
+//  Copyright © 2016 Kevin Barabash. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
